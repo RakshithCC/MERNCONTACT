db436de07aae1028c4bd77eaac12ebd7bdd293e9
RakshithCC <rakshithcc670@gmail.com>
2023-05-22 18:23:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+require("dotenv").config({ path: "./config/config.env" });
+const express = require("express");
+const morgan = require("morgan");
+
+const connectDB = require("./config/db");
+
+const auth = require("./middlewares/auth");
+
+const app = express();
+
+// middlewares
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(require("cors")());
+
+// routes
+app.use("/api", require("./routes/auth"));
+app.use("/api", require("./routes/contact"));
+
+// server configurations.
+const PORT = process.env.PORT || 8000;
+app.listen(PORT,async()=>{
+    try {
+        await connectDB();
+    console.log(`Server listening on the port:${PORT}`)
+    } catch (error) {
+        console.log(err);
+    }
+    
+});
